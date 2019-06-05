d32c530133df7f7217a08dc76a96e74c551b2873
Tom Blauwendraat <antiflu@gmail.com>
2019-06-05 16:48:15 +0200

    [FIX] some fixes

--- models/account_followup.py
+++ models/account_followup.py
@@ -1,23 +1,7 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    OpenERP, Open Source Management Solution
-#    Copyright (C) 2004-2010 Tiny SPRL (<http://tiny.be>).
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# Copyright 2004-2010 Tiny SPRL (<http://tiny.be>).
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
 import time
 from collections import defaultdict
 from odoo import api, tools
@@ -185,8 +169,8 @@ class AccountFollowupStat(models.Model):
     credit = fields.Float('Credit', readonly=True)
     company_id = fields.Many2one('res.company', 'Company', readonly=True)
     blocked = fields.Boolean('Blocked', readonly=True)
-    period_id = fields.Many2one('account.period', 'Period', readonly=True)
 
+    # TODO: do something with OCA account_fiscal_year or date_range modules
     # def search(self, args, offset=0, limit=None, order=None,
     #             context=None, count=False):
     #     for arg in args:
@@ -206,7 +190,7 @@ class AccountFollowupStat(models.Model):
     #             domain.append(['period_id','in',ids])
     #             domain.remove(arg)
     #     return super(AccountFollowupStat, self).read_group(domain, *args, **kwargs)
-    #
+
     @api.model_cr
     def init(self):
         tools.drop_view_if_exists(self._cr, 'account_followup_stat')
@@ -234,5 +218,3 @@ class AccountFollowupStat(models.Model):
                 GROUP BY
                     l.id, l.partner_id, l.company_id, l.blocked
             )""")
-
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
